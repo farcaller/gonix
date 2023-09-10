f644276bd7ea2b14dabeae01d965d6b555287106
Vladimir Pouzanov <farcaller@gmail.com>
2023-09-10 21:02:29 +0200

    Add the package description

--- pkg/gonix/gonix.go
+++ pkg/gonix/gonix.go
@@ -1,3 +1,6 @@
+// Package gonix provides bindings to the nix APIs. It can work with any store
+// supported by nix, including working with `/nix/store` directly or over the
+// nix-daemon.
 package gonix
 
 // #cgo pkg-config: nix-expr-c
@@ -8,7 +11,7 @@ package gonix
 import "C"
 
 func init() {
+	C.nix_libutil_init(nil)
 	C.nix_libexpr_init(nil)
 	C.nix_libstore_init(nil)
-	C.nix_libutil_init(nil)
 }
